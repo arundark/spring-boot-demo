4b666cb72f06f04257924d1c6385577721e4dd4a
ArunArangan <arundark48@gmail.com>
2023-10-08 18:41:23 +0530

    @RestController added

--- src/main/java/com/dailycodebuffer/springbootdemo/controller/HomeController.java
+++ src/main/java/com/dailycodebuffer/springbootdemo/controller/HomeController.java
@@ -1,10 +1,10 @@
 package com.dailycodebuffer.springbootdemo.controller;
 
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-@Controller
-@ResponseBody
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
 
     @RequestMapping("/")
