eb633b95a1d8e522bd0b5f161a5a08977af9c567
ArunArangan <arundark48@gmail.com>
2023-10-09 20:58:30 +0530

    EmployeeService and impl created

---
+++ src/main/java/com/dailycodebuffer/springbootdemo/service/EmployeeService.java
@@ -0,0 +1,4 @@
+package com.dailycodebuffer.springbootdemo.service;
+
+public interface EmployeeService {
+}
---
+++ src/main/java/com/dailycodebuffer/springbootdemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,7 @@
+package com.dailycodebuffer.springbootdemo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+}
