fe4a881f44c4aa4e2c050ddd6bd486512ef052e7
ArunArangan <arundark48@gmail.com>
2023-10-09 09:51:33 +0530

    User model created

--- src/main/java/com/dailycodebuffer/springbootdemo/controller/HomeController.java
+++ src/main/java/com/dailycodebuffer/springbootdemo/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.dailycodebuffer.springbootdemo.controller;
 
 
+import com.dailycodebuffer.springbootdemo.model.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,15 @@ public class HomeController {
     @RequestMapping("/")
     public String home(){
         return "hello";
+    }
+
+    @RequestMapping("/user")
+    public User user(){
+        User user = new User();
+        user.setId("1");
+        user.setName("arun");
+        user.setEmailId("arunarangan10@gmai");
+
+        return user;
     }
 }
